72a6fb92196a1be95f8b6dfcbeaf6a5bef208447
COSTA Laurent <lacosta@clust-n1.domensai.ecole>
2025-06-18 10:24:58 +0200

    debut script analyse pour test commit

---
+++ scripts/analyse_laurent.R
@@ -0,0 +1,10 @@
+# Chargement des bibliothèques et du fichier
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(tidyr)
+library(forcats)
+
+bdd <- read_rds("data/bdd_2015_2024.rds")
+                    
+
