3efe80c531a4ac8a98e0e84a8ce4e602eafb650c
LESIEUR Christophe <clesieur@DOMENSAI.ECOLE>
2025-09-09 18:05:24 +0200

    xvxdvsdv

--- renv.R
+++ renv.R
@@ -20,4 +20,25 @@ renv::snapshot()
 renv::restore(prompt = FALSE)  # réinstalle les deps pour R 4.5
 renv::rebuild()                # (optionnel) reconstruit proprement
 renv::snapshot()               # met à jour renv.lock (Version R = 4.5.1)
-Y
+
+renv::install("readxl")
+renv::snapshot()
+
+quarto::quarto_render()
+
+
+pkgs <- c(
+  "arrow","dplyr","here","DT","readr","readxl",
+  "knitr","kableExtra","scales","forcats","tidyr","ggplot2"
+)
+
+missing <- pkgs[!vapply(pkgs, requireNamespace, logical(1), quietly = TRUE)]
+if (length(missing)) {
+  stop("Packages manquants dans l'env renv: ", paste(missing, collapse = ", "),
+       call. = FALSE)
+}
+
+invisible(lapply(pkgs, library, character.only = TRUE))
+
+renv::install(missing)   # si le message te dit lesquels manquent
+renv::snapshot()
