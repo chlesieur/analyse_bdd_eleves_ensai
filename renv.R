c4ff9fb3021f8687f9adb6f21642d25163714815
LESIEUR Christophe <clesieur@DOMENSAI.ECOLE>
2025-09-09 16:06:31 +0200

    Alleeeeez

--- renv.R
+++ renv.R
@@ -1,4 +1,14 @@
 install.packages("renv")
 renv::init()
 
+renv::restore()      # installe ce qui est déjà dans renv.lock mais pas présent
+renv::snapshot()     # enregistre dans renv.lock tous les packages "used" (crosstalk, DBI, etc.)
+
+# 4) Vérifier que tout est propre
+renv::status()
+
+install.packages(c("DBI", "sp"))
+
+renv::snapshot()
+
 renv::status()
